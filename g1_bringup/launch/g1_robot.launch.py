e985c8026f52ee03527f77407e8b76b2fa2a2b82
Sam <sam@aukilabs.com>
2025-02-18 12:55:23 +0800

    added realsense camera launch

--- g1_bringup/launch/g1_robot.launch.py
+++ g1_bringup/launch/g1_robot.launch.py
@@ -8,6 +8,8 @@ import os
 def generate_launch_description():
     # Sensors
     mid360_file = os.path.join(FindPackageShare('g1_bringup').find('g1_bringup'), 'launch', 'sensors', 'mid360.launch.py')
+    rs_file = os.path.join(FindPackageShare('realsense2_camera').find('realsense2_camera'), 'launch', 'rs_launch.py')
+    rs_config = os.path.join(FindPackageShare('g1_bringup').find('g1_bringup'), 'config', 'sensors', 'realsense.yaml')
 
     # Description
     g1_description_file = os.path.join(FindPackageShare('g1_description').find('g1_description'), 'launch', 'g1_description.launch.py')
@@ -15,6 +17,14 @@ def generate_launch_description():
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(mid360_file)
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(rs_file),
+            launch_arguments={
+                'camera_name': 'd435',
+                'camera_namespace': 'rs_camera',
+                'config_file': rs_config
+            }.items()
         ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(g1_description_file)
